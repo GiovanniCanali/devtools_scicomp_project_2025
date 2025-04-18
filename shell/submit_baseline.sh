1add1ccb7896745f186b1145a07014348aaa7191
FilippoOlivo <filippo@filippoolivo.com>
2025-04-18 11:54:59 +0200

    Add experiments

---
+++ shell/submit_baseline.sh
@@ -0,0 +1,5 @@
+python scripts/run.py --config_file experiments/s4.yaml --fit True
+python scripts/run.py --config_file experiments/s4_low_rank.yaml --fit True
+python scripts/run.py --config_file experiments/s4d_inv.yaml --fit True
+python scripts/run.py --config_file experiments/s4d_real.yaml --fit True
+python scripts/run.py --config_file experiments/s6.yaml --fit True
---
+++ shell/submit_mamba.sh
@@ -0,0 +1,4 @@
+python scripts/run.py --config_file experiments/mamba_s4d_real.yaml --fit True
+python scripts/run.py --config_file experiments/mamba_s4d_inv.yaml --fit True
+python scripts/run.py --config_file experiments/mamba_s4_low_rank.yaml --fit True
+python scripts/run.py --config_file experiments/mamba_s6.yaml --fit True
