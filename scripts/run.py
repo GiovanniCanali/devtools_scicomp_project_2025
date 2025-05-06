Fix normalization and clamp

scripts/run.py
@@ -1,5 +1,5 @@
 import torch
 from ssm.cli import TrainingCLI
-
+torch.autograd.set_detect_anomaly(True)
 cli = TrainingCLI()
 cli()

ssm/model/block/s6_block.py
@@ -32,7 +32,6 @@ class DeltaNetwork(torch.nn.Module):
             dt_max=dt_max,
             inverse_softplus=True,
         )
-
         self.project = torch.nn.Linear(dt_rank, model_dim, bias=True)
         dt_init_std = dt_rank**-0.5 * dt_scale
         torch.nn.init.uniform_(self.project.weight, -dt_init_std, dt_init_std)
@@ -148,7 +147,7 @@ class S6Block(torch.nn.Module):
         B, C = self.linear(x).chunk(2, dim=-1)
 
         # Compute dt
-        dt = self.delta_net(x)
+        dt = self.delta_net(x).clamp(min=1e-7, max=1e6)
 
         # Discretize A and B
         A_bar, B_bar = self._discretize(self.A, B, dt)

ssm/model/mamba.py
@@ -27,6 +27,9 @@ class Mamba(torch.nn.Module):
 
         self.model_dim = kwargs["model_dim"]
         self.mamba_blocks = torch.nn.Sequential(*mamba_blocks)
+        self.norm_layer = (
+            torch.nn.RMSNorm(kwargs["model_dim"]) if normalization else None
+        )
 
     def forward(self, x):
         """
@@ -37,4 +40,7 @@ class Mamba(torch.nn.Module):
         :return: Output tensor.
         :rtype: torch.Tensor
         """
-        return self.mamba_blocks(x)
+        x = self.mamba_blocks(x)
+        if self.norm_layer is not None:
+            x = self.norm_layer(x)
+        return x

ssm/model/s4.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn import LayerNorm
+from torch.nn.modules.normalization import RMSNorm
 from .block import S4BaseBlock, S4LowRankBlock, S4DBlock
 from .block.mixing_block import MixingBlock
 
@@ -82,7 +83,7 @@ class S4(torch.nn.Module):
         layers = []
         for _ in range(n_layers):
             if normalization:
-                layers.append(LayerNorm(model_dim, elementwise_affine=False))
+                layers.append(RMSNorm(model_dim, elementwise_affine=False))
             layers.append(
                 block_class(
                     model_dim=model_dim,
@@ -94,6 +95,11 @@ class S4(torch.nn.Module):
             layers.append(activation())
             layers.append(MixingBlock(model_dim))
         self.layers = torch.nn.Sequential(*layers)
+        self.norm_layer = (
+            RMSNorm(model_dim, elementwise_affine=False)
+            if normalization
+            else None
+        )
 
     def forward(self, x):
         """
@@ -103,7 +109,10 @@ class S4(torch.nn.Module):
         :return: The output tensor.
         :rtype: torch.Tensor
         """
-        return self.layers(x)
+        x = self.layers(x)
+        if self.norm_layer is not None:
+            x = self.norm_layer(x)
+        return x
 
     def change_forward(self, method):
         """

ssm/model/s6.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn import LayerNorm
+from torch.nn.modules.normalization import RMSNorm
 from .block import S6Block
 from .block.mixing_block import MixingBlock
 
@@ -62,7 +63,7 @@ class S6(torch.nn.Module):
         layers = []
         for _ in range(n_layers):
             if normalization:
-                layers.append(LayerNorm(model_dim, elementwise_affine=False))
+                layers.append(RMSNorm(model_dim, elementwise_affine=False))
             layers.append(
                 S6Block(
                     model_dim=model_dim,
@@ -74,6 +75,11 @@ class S6(torch.nn.Module):
             layers.append(activation())
             layers.append(MixingBlock(model_dim))
         self.layers = torch.nn.Sequential(*layers)
+        self.norm_layer = (
+            RMSNorm(model_dim, elementwise_affine=False)
+            if normalization
+            else None
+        )
 
     def forward(self, x):
         """
@@ -83,4 +89,7 @@ class S6(torch.nn.Module):
         :return: The output tensor.
         :rtype: torch.Tensor
         """
-        return self.layers(x)
+        x = self.layers(x)
+        if self.norm_layer is not None:
+            x = self.norm_layer(x)
+        return x